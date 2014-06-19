45eb02fbb22cfea25d67c3c226816eab327bd17d
Ajeet Jha <ajeetjha1@gmail.com>
2014-06-19 16:34:13 +0530

    BST : Adding search feature
    Signed-off-by: Ajeet Jha <ajeetjha1@gmail.com>

--- memoryCodes/binarySearchTree.py
+++ memoryCodes/binarySearchTree.py
@@ -16,7 +16,6 @@ class binarySearchTree:
 
     def insertNode (self, parNode, num):
         if parNode != None:
-            print parNode.data
             if num < parNode.data :
                 if parNode.leftNode == None:
                     parNode.leftNode = node(num)
@@ -37,6 +36,44 @@ class binarySearchTree:
         if node.rightNode != None:
             self.printInorder (node.rightNode)
 
+    def searchElement (self, node, num):
+        ''' Searches integer number in the BST '''
+        if node.data == num:
+            return True
+        elif num < node.data :
+            if node.leftNode == None:
+                return False
+            else:
+                return self.searchElement (node.leftNode, num)
+        else :
+            if node.rightNode == None:
+                return False
+            else :
+                return self.searchElement (node.rightNode, num)
+
+def main():
+    print "Enter\nCreate Tree (1) \nPrintInorder (2)\nInsert Element(3) \nSearch element (4)"
+    while (1) :
+        case = int(raw_input("ENTER: "))
+        if case == 1:
+            num = int(raw_input ("Root : "))
+            tree = binarySearchTree(num)
+        elif case == 2:
+            tree.printInorder (tree.root)
+        elif case == 3:
+            num = int(raw_input ("Enter num to insert : "))
+            tree.insertNode (tree.root, num)
+        elif case == 4:
+            num = int(raw_input ("Enter num to find : "))
+            if tree.searchElement (tree.root, num):
+                print "FOUND"
+            else:
+                print "NOT Found"
+
+if __name__ == "__main__":
+    main()
+
+'''
 tree = binarySearchTree(20)
 tree.insertNode (tree.root, 15)
 tree.insertNode (tree.root, 25)
@@ -45,7 +82,6 @@ tree.insertNode (tree.root, 35)
 print "Inorder : "
 tree.printInorder(tree.root)
 
-'''
 print "Testing :"
 print "Root : " + str(tree.root.data)
 print "Left :" + str(tree.root.leftNode.data)
